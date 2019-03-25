7c720558f3cdab9d9773af87dccdb6ff147573fb
Khamidulla Inoyatov <khamiudlla@inoyatov.com>
2019-03-25 21:58:57 +0900

    Models updated

--- librarium/api/models.py
+++ librarium/api/models.py
@@ -1,23 +1,60 @@
 from django.db import models
 
 
-class BaseModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated = models.DateTimeField(
+        auto_now=True
+    )
 
     class Meta:
         abstract = True
 
 
-class Author(BaseModel):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class Author(TimeStampedModel):
+    first_name = models.CharField(
+        max_length=100,
+    )
+    last_name = models.CharField(
+        max_length=100,
+    )
+    middle_name = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+    userpic = models.ImageField(
+        upload_to='userpics'
+    )
 
 
-class Book(BaseModel):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+class Publisher(TimeStampedModel):
+    name = models.CharField(
+        max_length=100
+    )
+    description = models.TextField(
+        max_length=600
+    )
 
 
-class Publisher(BaseModel):
-    name = models.CharField(max_length=50)
+class Book(TimeStampedModel):
+    title = models.CharField(
+        max_length=150
+    )
+    description = models.TextField(
+        max_length=600
+    )
+    upload = models.FileField(
+        upload_to='books/'
+    )
+    cover = models.ImageField(
+        upload_to='books/'
+    )
+    published_date = models.DateField()
+    authors = models.ManyToManyField(
+        Author,
+    )
+    publisher = models.ManyToManyField(
+        Publisher,
+    )
