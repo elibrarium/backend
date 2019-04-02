9a484144cf1a96a57ed36de53ed4453e17064b0d
Khamidulla Inoyatov <khamiudlla@inoyatov.com>
2019-04-02 20:21:44 +0900

    JWT configured

--- librarium/api/urls.py
+++ librarium/api/urls.py
@@ -1,4 +1,7 @@
+from django.urls import path
+
 from rest_framework import routers
+from rest_framework_jwt import views as jwt_views
 
 from .views import BookModelViewSet
 from .views import AuthorModelViewSet
@@ -12,3 +15,23 @@ router.register(r'authors', AuthorModelViewSet)
 router.register(r'publishers', PublisherModelViewSet)
 
 urlpatterns = router.urls
+
+
+urlpatterns += [
+    # Authentication endpoints
+    path(
+        "auth/jwt/token/obtain/",
+        jwt_views.ObtainJSONWebToken.as_view(),
+        name="obtain-token",
+    ),
+    path(
+        "auth/jwt/token/refresh/",
+        jwt_views.RefreshJSONWebToken.as_view(),
+        name="refresh-token",
+    ),
+    path(
+        "auth/jwt/token/verify/",
+        jwt_views.VerifyJSONWebToken.as_view(),
+        name="verify-token",
+    ),
+]
--- librarium/librarium/settings/base.py
+++ librarium/librarium/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,6 +133,21 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
+
+JWT_AUTH = {
+    "JWT_EXPIRATION_DELTA": datetime.timedelta(days=1),
+    "JWT_AUTH_HEADER_PREFIX": "Token",
+    "JWT_ALLOW_REFRESH": True,
 }
